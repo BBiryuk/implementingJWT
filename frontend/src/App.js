3861171cf62e5cb81108f5688d663b06b3b726ae
John <birukovie@gmail.com>
2023-09-12 22:03:04 +0300

    Add private routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import Header from './components/Header';
+import PrivateRoute from './utils/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route element={<PrivateRoute/>}>
+            <Route element={<MainPage/>} path='/' exact/>
+          </Route>
+          <Route element={<LoginPage/>} path="/login"/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>
+        <p>Logged in</p>
+    </div>
+  )
+}
+
+export default MainPage
